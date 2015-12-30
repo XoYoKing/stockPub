83749490e926c33db6323d9a2abf4de0a20cb82b
wanghan <wh85125@163.com>
2015-12-30 22:37:53 +0800

    #1

--- utility/commonFunc.js
+++ utility/commonFunc.js
@@ -1,7 +1,7 @@
 var logger = global.logger;
 var config = require('../config');
 var http = require('http');
-
+var gs = require('nodegrass');
 
 
 exports.analyzeMessage = function(htmlData){
@@ -72,18 +72,20 @@ exports.getStockInfoFromAPI = function(stock_code, callback) {
 	var stockAPI = config.stockDataInterface.url + mrkDesc + stock_code;
 	logger.debug(stockAPI);
 
-	http.get(stockAPI, function(res) {
-		if (res.statusCode == 200) {
-			var htmlData = "";
-			res.on('data', function(data) {
-				htmlData += data;
-			});
+	gs.get(stockAPI, function(data) {
+		// if (res.statusCode == 200) {
+		// 	var htmlData = "";
+		// 	res.on('data', function(data) {
+		// 		htmlData += data;
+		// 	});
+        //
+		// 	res.on('end', function() {
+		// 		callback(true, htmlData);
+		// 	});
+		// }
+        console.log(data);
 
-			res.on('end', function() {
-				callback(true, htmlData);
-			});
-		}
-	}).on('error', function(e) {
+	}, 'gbk').on('error', function(e) {
 		logger.error("Got error: " + e.message);
         callback(false, e.message);
 	});
