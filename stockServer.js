c633b1cc44013b0219f64a269a0cf70274027f9b
wanghan <wh85125@163.com>
2015-12-31 16:11:52 +0800

    #1

--- stockServer.js
+++ stockServer.js
@@ -10,11 +10,13 @@ var email = require('./utility/emailTool');
 var stockRouter = require('./router/stockRouter.js');
 var config = require('./config');
 var port = config.stockServerInfo.port;
+var morgan = require('morgan');
+
 
 process.on('uncaughtException', function(err) {
-	logger.error('worker exception: ' + err.stack);
-	email.sendMail('Caught exception: ' + err.stack,
-		'stockServer process failed');
+    logger.error('worker exception: ' + err.stack);
+    email.sendMail('Caught exception: ' + err.stack,
+        'stockServer process failed');
 });
 
 global.app = express(); //创建express实例
@@ -27,12 +29,35 @@ global.app.set('imagePath', path.join(__dirname, 'images'));
 
 global.app.use(bodyParser.json());
 global.app.use(bodyParser.urlencoded({
-	extended: false
+    extended: false
 }));
 
 global.app.use(express.static(__dirname + '/css'));
 global.app.use(express.static(__dirname + '/js'));
 global.app.use(express.static(__dirname + '/images'));
+
+// create a rotating write stream
+var accessLogStream = fileStreamRotator.getStream({
+    filename: path.join(process.env.HOME,
+        'stocklogs', '/stockServer_access_%DATE%.log'),
+    frequency: 'daily',
+    verbose: false,
+    date_format: "YYYY-MM-DD"
+});
+
+global.app.use(morgan('short', {
+    stream: accessLogStream
+}));
+
+// 该路由使用的中间件
+global.app.use(function(req, res, next) {
+    req.body.sq = Date.now();
+    log.info(JSON.stringify(req.body), log.getFileNameAndLineNum(
+            __filename),
+        req.body.sq);
+    next();
+});
+
 
 global.app.use('/stock', stockRouter);
 
