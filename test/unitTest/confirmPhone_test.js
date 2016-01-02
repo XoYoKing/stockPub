b8c1f6055d240c4ddcc42c21a2cddc88a8c61a66
wanghan <wh85125@163.com>
2016-01-02 16:08:31 +0800

    #5

--- test/unitTest/confirmPhone_test.js
+++ test/unitTest/confirmPhone_test.js
@@ -1,3 +1,5 @@
+var should = require('should');
+
 var Json = {
     childpath: '/user/confirmPhone',
     user_phone: '18516053712',
@@ -5,5 +7,6 @@ var Json = {
 
 var runner = require('./unitTestRunner.js');
 runner.runTest(Json, Json.childpath, function(err, body){
-    
+    should.not.exist(err);
+    body.code.should.equal(1040);
 });
