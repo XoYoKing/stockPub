9dfeac80262de80b4427530b9cfee49b7bb3c654
wanghan <wh85125@163.com>
2016-01-02 18:51:44 +0800

    #5

--- test/unitTest/register_test.js
+++ test/unitTest/register_test.js
@@ -2,13 +2,15 @@ var should = require('should');
 var constant = require('../../utility/constant.js');
 
 var Json = {
-    childpath: '/user/confirmPhone',
+    childpath: '/user/register',
     user_phone: '18516053712',
+    user_certificate_code: '12345'
 };
 
 var runner = require('./unitTestRunner.js');
 runner.runTest(Json, Json.childpath, function(err, body){
     should.not.exist(err);
-    body.code.should.be.equalOneOf(constant.returnCode.CERTIFICATE_CODE_SEND,
-        constant.returnCode.CERTIFICATE_CODE_SENDED);
+    body.code.should.be.equalOneOf(constant.returnCode.CERTIFICATE_CODE_NOT_MATCH,
+        constant.returnCode.REGISTER_SUCCESS, constant.returnCode.PHONE_EXIST,
+        constant.returnCode.ERROR);
 });
