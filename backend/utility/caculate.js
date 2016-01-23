cca02795cdcd7553e5730259d6236b0cfef8f03b
wanghan <wh85125@163.com>
2016-01-23 19:49:49 +0800

    #19

--- backend/utility/caculate.js
+++ backend/utility/caculate.js
@@ -6,9 +6,17 @@ var stockOperation = require('../databaseOperation/stockOperation');
 exports.caculateAllUserYield = function(){
     userOperation.updateAllUserTotalYield(function(flag, result){
         if(flag){
-            logger.info('success updateAllUserTotalYield', logger.getFileNameAndLineNum(__filename));
+            if(logger == null){
+                console.log('success updateAllUserTotalYield');
+            }else{
+                logger.info('success updateAllUserTotalYield', logger.getFileNameAndLineNum(__filename));
+            }
         }else{
-            logger.error(result, logger.getFileNameAndLineNum(__filename));
+            if(logger == null){
+                console.log(result);
+            }else{
+                logger.error(result, logger.getFileNameAndLineNum(__filename));
+            }
         }
     });
 }
