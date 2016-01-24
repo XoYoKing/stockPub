ced15693ba11d8f8fc26db209db03c56779f117c
wanghan <wh85125@163.com>
2016-01-24 21:20:24 +0800

    #19

--- backend/databaseOperation/stockOperation.js
+++ backend/databaseOperation/stockOperation.js
@@ -27,7 +27,7 @@ exports.getFollowLookInfo = function(reqbody, callback){
     ' where a.user_id = ? and a.followed_user_id = b.user_id ' +
     ' and a.followed_user_id = c.user_id ' +
     ' and b.look_status = 1 ' +
-    ' and b.stock_code = d.stock_code and b.look_timestamp<? order by b.look_update_timestamp desc limit 10';
+    ' and b.stock_code = d.stock_code and b.look_update_timestamp<? order by b.look_update_timestamp desc limit 10';
     conn.executeSql(sql, [reqbody.user_id, reqbody.look_timestamp], callback);
 
 }
