daaa2ca4e3df46c6c45a683bf63d9de052b73750
wanghan <wh85125@163.com>
2016-01-29 16:44:13 +0800

    #19

--- backend/stockSchedule.js
+++ backend/stockSchedule.js
@@ -32,9 +32,9 @@ schedule.scheduleJob('5 16 * * 1-5', function(){
 });
 
 //开市前删除now表中数据
-schedule.scheduleJob('42 16 * * 1-5', function(){
+schedule.scheduleJob('45 16 * * 1-5', function(){
     log.info("delete stock now data", log.getFileNameAndLineNum(__filename));
-    crawl.emptyStockNowInfo();
+    //crawl.emptyStockNowInfo();
     //crawl.emptyMarketIndexNowInfo();
 });
 
