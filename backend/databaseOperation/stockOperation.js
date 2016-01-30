49a69bed6a4776326c0d868395e0c39fdb8626e0
wanghan <wh85125@163.com>
2016-01-31 00:25:22 +0800

    #19

--- backend/databaseOperation/stockOperation.js
+++ backend/databaseOperation/stockOperation.js
@@ -26,7 +26,6 @@ exports.getFollowLookInfo = function(reqbody, callback){
     var sql = 'select b.*, c.user_name, c.user_facethumbnail, d.stock_name from user_follow_base_info a, stock_look_info b, user_base_info c, stock_base_info d' +
     ' where a.user_id = ? and a.followed_user_id = b.user_id ' +
     ' and a.followed_user_id = c.user_id ' +
-    ' and b.look_status = 1 ' +
     ' and b.stock_code = d.stock_code and b.look_update_timestamp<? order by b.look_update_timestamp desc limit 10';
     conn.executeSql(sql, [reqbody.user_id, reqbody.look_update_timestamp], callback);
 
