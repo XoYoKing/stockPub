b13024c2a9e6dd3cf18ca3c37cb6d6973830acde
wanghan <wh85125@163.com>
2016-02-03 10:20:00 +0800

    #19

--- backend/stockSchedule.js
+++ backend/stockSchedule.js
@@ -3,55 +3,50 @@ var log = require('./log.js');
 log.SetLogFileName(path.join(process.env.HOME, 'stocklogs/stockSchedule_'));
 global.logger = log; // 设置全局
 
-var schedule = require("node-schedule");
 var crawl = require('./stockCrawl.js');
 var email = require('./utility/emailTool');
 var caculate = require('./utility/caculate');
 
-log.info("run schedule", log.getFileNameAndLineNum(__filename));
+var cronJob = require('cron').CronJob;
 
 
-//实时行情-上午-每20秒
-schedule.scheduleJob('*/20 * * * * *', function(){
-    log.info("test", log.getFileNameAndLineNum(__filename));
-    //crawl.startCrawlStockNow();
-    //crawl.startCrawlMarket();
-});
-
+log.info("run CronJob", log.getFileNameAndLineNum(__filename));
 
 //实时行情-上午-每20秒
-schedule.scheduleJob('*/20 * 9-11 * * 1-5', function(){
+new CronJob('*/20 * 9-11 * * 1-5', function() {
     log.info("stock crawl Now start", log.getFileNameAndLineNum(__filename));
     crawl.startCrawlStockNow();
     crawl.startCrawlMarket();
-});
+}, null, true);
 
-//实时行情-下午-每20秒
-schedule.scheduleJob('*/20 * 13-14 * * 1-5', function(){
-    log.info("stock crawl Now start", log.getFileNameAndLineNum(__filename));
-    crawl.startCrawlStockNow();
-    crawl.startCrawlMarket();
-});
 
-//日终行情
-schedule.scheduleJob('5 16 * * 1-5', function(){
-    log.info("stock crawl day start", log.getFileNameAndLineNum(__filename));
-    crawl.startGetAllStockInfo();
-    crawl.startCrawlMarketDay();
-});
 
-//开市前删除now表中数据
-schedule.scheduleJob('25 9 * * 1-5', function(){
-    log.info("delete stock now data", log.getFileNameAndLineNum(__filename));
-    crawl.emptyStockNowInfo();
-    crawl.emptyMarketIndexNowInfo();
-});
-
-//日终计算用户总收益率
-schedule.scheduleJob('50 23 * * *', function(){
-    log.info('caculate yield for all user', log.getFileNameAndLineNum(__filename));
-    caculate.caculateAllUserYield();
-});
+// //实时行情-下午-每20秒
+// schedule.scheduleJob('*/20 * 13-14 * * 1-5', function(){
+//     log.info("stock crawl Now start", log.getFileNameAndLineNum(__filename));
+//     crawl.startCrawlStockNow();
+//     crawl.startCrawlMarket();
+// });
+//
+// //日终行情
+// schedule.scheduleJob('5 16 * * 1-5', function(){
+//     log.info("stock crawl day start", log.getFileNameAndLineNum(__filename));
+//     crawl.startGetAllStockInfo();
+//     crawl.startCrawlMarketDay();
+// });
+//
+// //开市前删除now表中数据
+// schedule.scheduleJob('25 9 * * 1-5', function(){
+//     log.info("delete stock now data", log.getFileNameAndLineNum(__filename));
+//     crawl.emptyStockNowInfo();
+//     crawl.emptyMarketIndexNowInfo();
+// });
+//
+// //日终计算用户总收益率
+// schedule.scheduleJob('50 23 * * *', function(){
+//     log.info('caculate yield for all user', log.getFileNameAndLineNum(__filename));
+//     caculate.caculateAllUserYield();
+// });
 
 process.on('uncaughtException', function(err) {
     log.error('schedule process Caught exception: ' +
