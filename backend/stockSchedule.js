2eee52d45d59d4fd013c6a8e4720c168cd512ff2
wanghan <wh85125@163.com>
2016-02-03 10:28:01 +0800

    #19

--- backend/stockSchedule.js
+++ backend/stockSchedule.js
@@ -21,32 +21,33 @@ new cronJob('*/20 * 9-11 * * 1-5', function() {
 
 
 
-// //实时行情-下午-每20秒
-// schedule.scheduleJob('*/20 * 13-14 * * 1-5', function(){
-//     log.info("stock crawl Now start", log.getFileNameAndLineNum(__filename));
-//     crawl.startCrawlStockNow();
-//     crawl.startCrawlMarket();
-// });
+//实时行情-下午-每20秒
+new cronJob('*/20 * 13-14 * * 1-5', function(){
+    log.info("stock crawl Now start", log.getFileNameAndLineNum(__filename));
+    crawl.startCrawlStockNow();
+    crawl.startCrawlMarket();
+}, null, true);
 //
-// //日终行情
-// schedule.scheduleJob('5 16 * * 1-5', function(){
-//     log.info("stock crawl day start", log.getFileNameAndLineNum(__filename));
-//     crawl.startGetAllStockInfo();
-//     crawl.startCrawlMarketDay();
-// });
+//日终行情
+new cronJob('00 5 15 * * 1-5', function(){
+    log.info("stock crawl day start", log.getFileNameAndLineNum(__filename));
+    crawl.startGetAllStockInfo();
+    crawl.startCrawlMarketDay();
+}, null, true);
+
 //
-// //开市前删除now表中数据
-// schedule.scheduleJob('25 9 * * 1-5', function(){
-//     log.info("delete stock now data", log.getFileNameAndLineNum(__filename));
-//     crawl.emptyStockNowInfo();
-//     crawl.emptyMarketIndexNowInfo();
-// });
+//开市前删除now表中数据
+new cronJob('00 25 9 * * 1-5', function(){
+    log.info("delete stock now data", log.getFileNameAndLineNum(__filename));
+    crawl.emptyStockNowInfo();
+    crawl.emptyMarketIndexNowInfo();
+}, null, true);
 //
-// //日终计算用户总收益率
-// schedule.scheduleJob('50 23 * * *', function(){
-//     log.info('caculate yield for all user', log.getFileNameAndLineNum(__filename));
-//     caculate.caculateAllUserYield();
-// });
+//日终计算用户总收益率
+new cronJob('00 50 23 * * *', function(){
+    log.info('caculate yield for all user', log.getFileNameAndLineNum(__filename));
+    caculate.caculateAllUserYield();
+}, null, true);
 
 process.on('uncaughtException', function(err) {
     log.error('schedule process Caught exception: ' +
