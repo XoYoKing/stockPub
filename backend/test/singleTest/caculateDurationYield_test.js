73fc7a39ccbe240afa4b2a199f0807f3f8e2edd4
wanghan <wh85125@163.com>
2016-02-04 11:29:00 +0800

    #19

--- backend/test/singleTest/caculateDurationYield_test.js
+++ backend/test/singleTest/caculateDurationYield_test.js
@@ -6,3 +6,8 @@ global.logger = log; // 设置全局
 var caculate = require('../../utility/caculate');
 
 caculate.caculateDurationYield();
+
+process.on('uncaughtException', function(err) {
+    log.error('process Caught exception: ' +
+        err.stack);
+});
