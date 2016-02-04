a5d43373211c8cf6bbb68c2ccef07f01e36a9ef4
wanghan <wh85125@163.com>
2016-02-04 11:22:57 +0800

    #19

--- backend/databaseOperation/stockOperation.js
+++ backend/databaseOperation/stockOperation.js
@@ -162,5 +162,5 @@ exports.insertStockLookYield = function(element, callback){
 
 exports.getStockDayInfoByDate = function(stock_code, date, callback){
     var sql = 'select* from stock_amount_info where stock_code = ? and date>=? order by date asc limit 1';
-    conn.executeSql(sql, [element.stock_code, element.date], callback);
+    conn.executeSql(sql, [stock_code, date], callback);
 }
