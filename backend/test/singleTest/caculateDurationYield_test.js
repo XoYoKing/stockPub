339acdc25849cf116bb686aff63ddf04faae09ea
wanghan <wh85125@163.com>
2016-02-04 11:30:11 +0800

    #19

--- backend/test/singleTest/caculateDurationYield_test.js
+++ backend/test/singleTest/caculateDurationYield_test.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var log = require('../../log.js');
-log.SetLogFileName('./'+__filename+'.log');
+log.SetLogFileName('test');
 global.logger = log; // 设置全局
 
 var caculate = require('../../utility/caculate');
