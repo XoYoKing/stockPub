d6c497afd8aea2b410765b1913a34ee855c13217
wanghan <wh85125@163.com>
2016-02-18 09:27:40 +0800

    #38

--- frontend/stockFront/stockFront/view/KLineCell.m
+++ frontend/stockFront/stockFront/view/KLineCell.m
@@ -56,8 +56,7 @@
 - (void)configureCell:(StockInfoModel*)model
 {
     if (isLoad == false) {
-        NSString* urlStr = [[NSString alloc] initWithFormat:@"%@%@%@%ld", [ConfigAccess serverDomain], @"/stock/kline?stock_code=&height=", model.stock_code, [KLineCell cellHeight]*3];
-        
+        NSString* urlStr = [[NSString alloc] initWithFormat:@"%@%@", [ConfigAccess serverDomain], [[NSString alloc] initWithFormat:@"/stock/kline?stock_code=%@&height=%ld", model.stock_code, [KLineCell cellHeight]*3]];
         NSLog(@"%@", urlStr);
         
         
