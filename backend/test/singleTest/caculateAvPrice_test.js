fc1f0dfc2effed6aac11112413a5ce47ca136f83
wanghan <wh85125@163.com>
2016-02-21 14:02:57 +0800

    #38

--- backend/test/singleTest/caculateAvPrice_test.js
+++ backend/test/singleTest/caculateAvPrice_test.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var log = require('../../log.js');
+var moment = require('moment');
 log.SetLogFileName('test');
 global.logger = log; // 设置全局
 
 var caculate = require('../../utility/caculate');
 
-caculate.caculateAvPrice(5);
-caculate.caculateAvPrice(10);
-caculate.caculateAvPrice(20);
+
+caculate.caculateAvPrice(5, moment().format('YYYY-MM-DD'));
+caculate.caculateAvPrice(10, moment().format('YYYY-MM-DD'));
+caculate.caculateAvPrice(20, moment().format('YYYY-MM-DD'));
