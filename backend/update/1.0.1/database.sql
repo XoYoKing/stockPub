22641ea6dda4943123b4d103f172adfb22372a68
wanghan <wh85125@163.com>
2016-02-24 16:17:53 +0800

    #38

--- backend/update/1.0.1/database.sql
+++ backend/update/1.0.1/database.sql
@@ -31,3 +31,15 @@ UPDATE `market_index_now_info` SET `timestamp_ms`  = `timestamp` *1000;
 UPDATE `market_index_now_info` SET `timestamp_ms` = `timestamp` ;
 
 UPDATE `market_index_day_info` SET `timestamp_ms` = `timestamp` ;
+
+#38
+ALTER TABLE `market_index_day_info`
+	ADD COLUMN `market_index_five_av_value` float NULL;
+
+#38
+ALTER TABLE `market_index_day_info`
+	ADD COLUMN `market_index_ten_av_value` float NULL;
+
+#38
+ALTER TABLE `market_index_day_info`
+	ADD COLUMN `market_index_twenty_av_value` float NULL;
