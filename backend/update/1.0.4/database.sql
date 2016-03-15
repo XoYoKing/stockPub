9d6b186e9a78c570d231bdad50907ec7a663dab7
wanghan <wh85125@163.com>
2016-03-15 14:16:54 +0800

    #69

---
+++ backend/update/1.0.4/database.sql
@@ -0,0 +1,11 @@
+#69
+CREATE TABLE `stock_talk_base_info` (
+	`talk_id` varchar(128) NOT NULL,
+	`talk_stock_code` varchar(32) NOT NULL,
+	`talk_user_id` varchar(128) NOT NULL,
+	`talk_to_user_id` varchar(128) NULL,
+	`talk_content` varchar(1024) NOT NULL,
+	`talk_date_time` varchar(32) NOT NULL,
+	`talk_timestamp_ms` bigint(32) NOT NULL,
+	PRIMARY KEY (`talk_id`)
+) ENGINE=InnoDB;
