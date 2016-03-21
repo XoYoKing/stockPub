a1903137c1de22e5d99f1bdd30feb912f5ff4fc5
wanghan <wh85125@163.com>
2016-03-21 11:28:55 +0800

    #69

---
+++ backend/test/singleTest/redisClient_test.js
@@ -0,0 +1,29 @@
+var redis = require("redis");
+var redisClient = redis.createClient({auth_pass:'here_dev'});
+redisClient.on("error", function (err) {
+    console.log(err);
+});
+
+setInterval(function(){
+    redisClient.get('test1', function(err, reply){
+        if (err) {
+            console.log(err);
+        }else{
+            console.log('key:test1 value:'+reply);
+        }
+    });
+    redisClient.get('test2', function(err, reply){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('key:test2 value:'+reply);
+        }
+    });
+    redisClient.get('test3', function(err, reply){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('key:test3 value:'+reply);
+        }
+    });
+}, 1000)
