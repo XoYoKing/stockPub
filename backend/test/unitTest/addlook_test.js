116e02843a7dadb6e2da44ca536ba2c3684196dc
wanghan <wh85125@163.com>
2016-03-23 18:24:27 +0800

    #74

--- backend/test/unitTest/addlook_test.js
+++ backend/test/unitTest/addlook_test.js
@@ -16,7 +16,7 @@ describe('addlook', function(){
         runner.runTest(Json, Json.childpath, function(err, body){
             should.not.exist(err);
             body.code.should.be.equalOneOf(constant.returnCode.LOOK_STOCK_EXIST,
-                constant.returnCode.SUCCESS, constant.returnCode.STOCK_NOT_EXIST);
+                constant.returnCode.SUCCESS, constant.returnCode.STOCK_NOT_EXIST, constant.returnCode.LOOK_STOCK_COUNT_OVER);
             done();
         });
     });
