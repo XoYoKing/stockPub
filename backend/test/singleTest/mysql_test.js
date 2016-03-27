2fbcd2a9cb6b9eee13ef8801458aadafa73817db
wanghan <wh85125@163.com>
2016-03-27 15:49:49 +0800

    test

---
+++ backend/test/singleTest/mysql_test.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host:process.env.dbhost,
+    user:process.env.dbuser,
+    password:process.env.dbpassword,
+    database:process.env.database
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
