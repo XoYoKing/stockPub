e69c721c4b4a686b008fd0c10cc469ea333bf738
wanghan <wh85125@163.com>
2016-03-27 15:59:24 +0800

    #test

--- backend/test/singleTest/mysql_test.js
+++ backend/test/singleTest/mysql_test.js
@@ -6,11 +6,12 @@ var connection = mysql.createConnection({
     database:process.env.database
 });
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
+connection.connect(function(err) {
+    if(!err){
+        connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+            if (err) throw err;
+            console.log('The solution is: ', rows[0].solution);
+            connection.end();
+        });
+    }
 });
-
-connection.end();
