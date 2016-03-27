fa43e6a60d07fd771bce533dec85640caa152e3c
wanghan <wh85125@163.com>
2016-03-27 16:54:51 +0800

    #test

--- backend/test/singleTest/redisPubSub_test.js
+++ backend/test/singleTest/redisPubSub_test.js
@@ -1,16 +1,21 @@
 var redis = require("redis"),
 client = redis.createClient({auth_pass:'here_dev'});
+
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
+var pub = redis.createClient({auth_pass:'here_dev'});
+
+//对发布消息的响应
 client.on('message', function(channel, message){
     console.log("sub channel " + channel + ": " + message);
 });
 
-
+//对自身订阅后的响应
 client.on("subscribe", function (channel, count) {
     console.log("sub channel " + channel + ": " + count);
+    pub.publish("first", "I am sending a message."); //向频道first发布信息
 });
 
 
