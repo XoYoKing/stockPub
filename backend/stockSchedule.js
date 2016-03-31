fd6354e9303e6e79dfbb7d0baf7f65b11be26cb2
wanghan <wh85125@163.com>
2016-03-31 23:52:34 +0800

    #93

--- backend/stockSchedule.js
+++ backend/stockSchedule.js
@@ -52,14 +52,14 @@ new cronJob('00 25 9 * * 1-5', function(){
 }, null, true);
 //
 
-//日终计算用户总收益率
-new cronJob('00 50 23 * * 1-5', function(){
+//收盘计算用户总收益率
+new cronJob('00 10 15 * * 1-5', function(){
     log.info('caculate yield for all user', log.getFileNameAndLineNum(__filename));
     caculate.caculateAllUserYield();
 }, null, true);
 
 //计算最近一周，最近一个月，最近一年的收益
-new cronJob('00 55 23 * * 1-5', function(){
+new cronJob('00 12 15 * * 1-5', function(){
     log.info('caculate yield for one week, one month, one year', log.getFileNameAndLineNum(__filename));
     caculate.caculateDurationYield();
 }, null, true);
