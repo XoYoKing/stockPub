8a2ae1715f284780cae24164a5828d23f6d7bd27
wanghan <wh85125@163.com>
2016-04-01 23:31:47 +0800

    #85

--- backend/test/singleTest/caculateUserDayYield_test.js
+++ backend/test/singleTest/caculateUserDayYield_test.js
@@ -1,5 +1,8 @@
 
 var caculate = require('../../utility/caculate');
+var log = require('../../log.js');
 
+log.SetLogFileName('test');
+global.logger = log; // 设置全局
 
 caculate.caculateUserDayYield();
