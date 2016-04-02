a38ba98e22237f41bc3e90532497b1ecf0937ea4
wanghan <wh85125@163.com>
2016-04-02 23:55:59 +0800

    #85

--- backend/update/1.0.5/updateMarketAvValue.js
+++ backend/update/1.0.5/updateMarketAvValue.js
@@ -6,37 +6,48 @@ log.SetLogFileName('updateMarketAvValue');
 stockOperation.getAllMarketIndexDay(function(flag, result){
     if(flag){
         result.forEach(function(e){
-            if(e.market_index_five_av_value === null){
+            {
                 console.log(e.market_code+' '+e.market_index_date+ " update ");
                 stockOperation.getMarketDayInfoLessNowDay(e.market_code, 20, e.market_index_date,
                 function(flag, result){
                     if(flag){
                         var totalTwenty = 0;
-                        for (var i = 0; i < result.length; i++) {
-                            totalTwenty+=result[i].market_index_value_now;
+                        if(result.length === 20){
+                            for (var i = 0; i < result.length; i++) {
+                                totalTwenty+=result[i].market_index_value_now;
+                            }
+                            totalTwenty/=result.length;
                         }
+
 
                         var totalTen = 0;
-                        for (var i = 10; i < result.length; i++) {
-                            totalTen+=result[i].market_index_value_now;
+                        if(result.length>=10){
+                            for (var i = 0; i<10; i++) {
+                                totalTen+=result[result.length - i - 1].market_index_value_now;
+                            }
+                            totalTen/=10;
                         }
+
 
                         var totalFive = 0;
-                        for (var i = 15; i < result.length; i++) {
-                            totalFive+=result[i].market_index_value_now;
+                        if(result.length>=5){
+                            for (var i = 0; i < 5; i++) {
+                                totalFive+=result[result.length - i - 1].market_index_value_now;
+                            }
+                            totalFive/=5;
                         }
 
-                        stockOperation.updateMarket5AvPrice(e.market_code, totalFive/5, e.market_index_date, function(flag, result){
+                        stockOperation.updateMarket5AvPrice(e.market_code, totalFive, e.market_index_date, function(flag, result){
                             if(!flag){
                                 log.error(result);
                             }
                         });
-                        stockOperation.updateMarket10AvPrice(e.market_code, totalTen/10, e.market_index_date, function(flag, result){
+                        stockOperation.updateMarket10AvPrice(e.market_code, totalTen, e.market_index_date, function(flag, result){
                             if(!flag){
                                 log.error(result);
                             }
                         });
-                        stockOperation.updateMarket20AvPrice(e.market_code, totalTwenty/20, e.market_index_date, function(flag, result){
+                        stockOperation.updateMarket20AvPrice(e.market_code, totalTwenty, e.market_index_date, function(flag, result){
                             if(!flag){
                                 log.error(result);
                             }
